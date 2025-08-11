61418a6bfbe433a3b966fc0365f3c6d1eb7917b1
divv919 <divyanshsoni919@gmail.com>
2025-08-11 21:34:39 +0530

    initial commit: Make HTTP server

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import * as http from "node:http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "content-type": "text/plain" });
+  res.end("This is a HTTP server");
+});
+
+server.listen(80, () => {
+  console.log("Running on 80");
+});
