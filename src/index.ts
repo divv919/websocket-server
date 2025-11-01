51158e0ec9e9a086504290a04d362660817ab7de
divv919 <divyanshsoni919@gmail.com>
2025-11-01 22:47:38 +0530

    fixes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import * as http from "node:http";
 import { SimpleWebSocketServer } from "./websocket.js";
 import type { Duplex } from "node:stream";
 
+const clients : Duplex[]= [] 
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "content-type": "text/plain" });
   res.end("This is an HTTP server");
@@ -10,12 +12,19 @@ const server = http.createServer((req, res) => {
 const wsServer = new SimpleWebSocketServer(server);
 
 wsServer.on("connect", (socket: Duplex) => {
+  clients.push(socket)
   console.log("Client connected");
 });
 
 wsServer.on("message", (socket: Duplex, message: string) => {
   console.log("Received message:", message);
-  wsServer.send(socket, "Hello from server!");
+  clients.forEach((socketToSendTo)=>{
+    if(socketToSendTo === socket){
+      return
+    }
+          wsServer.send(socketToSendTo, message);
+
+  })
 });
 
 server.listen(80, () => {
